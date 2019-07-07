ceef2c4c3fdb9312eb85951f539ffcd5cd312a45
Kamil Mahmood <kamil.mahmood@outlook.com>
2019-07-07 15:58:15 +0500

    renamed folder from src-c to extension

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-decrypter = Extension('_zipdecrypter', ['src-c/_zipdecryptermodule.c'])
+decrypter = Extension('_zipdecrypter', ['extension/_zipdecryptermodule.c'])
 
 setup(
       name='fastzipfile',
