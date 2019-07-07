0e057f245528b2f8c88673ea2edd41bb17aaf17c
Kamil Mahmood <kamil.mahmood@outlook.com>
2019-07-07 16:57:59 +0500

    fixed PyPi classifier values

--- setup.py
+++ setup.py
@@ -22,15 +22,15 @@ setup(
       classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Decryption :: Zip Decryption',
-        'License :: MIT License',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    keywords='zipfile decryption',
+    keywords='zip zipfile fastzip',
     python_requires='>=3.0, <4',
     py_modules=['fastzipfile'],
     ext_modules=[decrypter]
